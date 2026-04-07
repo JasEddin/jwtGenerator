1eaca8a89f7d88729bddef9d6309fdbf4a9f0d72
jasmine-se <jasmine.shihab.eddin@skandia.se>
2026-04-07 10:58:34 +0200

    Configure Swagger

--- jwtGenerator/Program.cs
+++ jwtGenerator/Program.cs
@@ -1,23 +1,51 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Scalar.AspNetCore;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
+// Add services
 builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+
+var key = "THIS_IS_MY_SECRET_KEY_123456"; // later move to config
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        IssuerSigningKey = new SymmetricSecurityKey(
+            Encoding.UTF8.GetBytes(key))
+    };
+});
+
 builder.Services.AddOpenApi();
-
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
+ 
+ 
+app.UseAuthentication(); // IMPORTANT
+app.UseAuthorization();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.MapScalarApiReference(); // Accessible at /scalar/v1
 }
 
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
+if(app.Environment.IsDevelopment())
+{
+    app.UseSwaggerUI(options => {
+        options.SwaggerEndpoint("/openapi/v1.json", "v1");
+    });
+}
 app.MapControllers();
 
 app.Run();
