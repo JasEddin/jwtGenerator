1636ba2813521b8b5b09f1a2cadf0033388f8c8e
jasmine-se <jasmine.shihab.eddin@skandia.se>
2026-04-09 09:31:35 +0200

    Styling, add Skandia styling and masking the token

--- jwtGenerator/jwtGenerator-ui/src/App.tsx
+++ jwtGenerator/jwtGenerator-ui/src/App.tsx
@@ -1,8 +1,8 @@
- 
+
 import { useState } from "react";
 import { generateToken } from "./jwtService";
+import "./App.css";
 
- 
 
 export type TokenResponse = {
   access_token: string;
@@ -11,19 +11,20 @@ export type TokenResponse = {
   expires_in: number;
 };
 enum Environment {
-          Stable = "stable",
-          Ct2 = "ct2",
-          Ct3 = "ct3",
-          Ct4 = "ct4",
-          Ct5 = "ct5",
-          U3 = "u3",
-          U4 = "u4",
-          U5 = "u5",
-          Lab = "lab"}
+  Stable = "stable",
+  Ct2 = "ct2",
+  Ct3 = "ct3",
+  Ct4 = "ct4",
+  Ct5 = "ct5",
+  U3 = "u3",
+  U4 = "u4",
+  U5 = "u5",
+  Lab = "lab"
+}
 
 function App() {
   const [env, setEnv] = useState("ct2" as Environment);
-  const [pnr, setPnr] = useState( "199001011234" );
+  const [pnr, setPnr] = useState("199001011234");
   const [result, setResult] = useState<TokenResponse | null>(null);
 
   const handleGenerate = async () => {
@@ -31,55 +32,81 @@ function App() {
 
       const res = await generateToken(pnr, env);
       setResult(res);
-    } catch (err) {   
+    } catch (err) {
       setResult(null);
     }
   };
 
+function prettierToken(token: string, start = 5, end = 5): string {
+  if (!token) return "";
+
+  if (token.length <= start + end) return token;
+
+  const first = token.slice(0, start);
+  const last = token.slice(-end);
+
+  return `${first} ••••••••••• ${last}`;
+}
+
   return (
-    <div style={{ padding: 20 }}>
-      <h2>Token Generator</h2>
+    <div className="app">
+      <div className="container">
+        <div style={{ padding: 20 }}>
+          <h2>Token Generator</h2>
 
-      <label>Environment: </label>
-      {/* dropdown for environment selection */}
-      <select value={env} onChange={(e) => setEnv(e.target.value as Environment)}>
-        {Object.values(Environment).map((envOption) => (
-          <option key={envOption} value={envOption}>
-            {envOption.toUpperCase()}
-          </option>
-        ))}
-      </select>
+          <label>Environment: </label>
+          {/* dropdown for environment selection */}
+          <select value={env} onChange={(e) => setEnv(e.target.value as Environment)}>
+            {Object.values(Environment).map((envOption) => (
+              <option key={envOption} value={envOption}>
+                {envOption.toUpperCase()}
+              </option>
+            ))}
+          </select>
 
-      <br /><br />
+          <br /><br />
 
-      <label> Personnummer : </label>
-      <input
-        value={pnr}
-        onChange={(e) => setPnr(e.target.value)}
-      />
-      <br /><br />
-      <button onClick={handleGenerate}>Generate Token</button>
-      <h3>Result:</h3>
-      <pre>{result ? JSON.stringify(result, null, 2) : "No result"  }</pre>
-
-           {result && (
-        <>
-          <h3>Access Token:</h3>
-          <textarea
-            style={{ width: "100%", height: 160 }}
-            value={result.access_token}
-            readOnly
+          <label> Personnummer : </label>
+          <input
+            value={pnr}
+            onChange={(e) => setPnr(e.target.value)}
           />
-
-          <button
-            onClick={() => {
-              navigator.clipboard.writeText(result.access_token);
-            }}
-          >
-            Copy Access Token
+          <br /><br />
+          <button className="btn-primary" onClick={handleGenerate}>
+            Generate token
           </button>
-        </>
-      )}
+          <h3>Response:</h3>
+          <p> access token: {result ? prettierToken(result.access_token) : "No token generated"}
+          {result && (
+              <button
+                onClick={() => {
+                  navigator.clipboard.writeText(result.access_token);
+                }}
+              >
+                {/* Copy Token  
+                the copy emoji */}
+                  📋
+              </button>)}
+          </p>
+          <p> scope: {result ? result.scope : "N/A"}</p>
+          <p> token type: {result ? result.token_type : "N/A"}</p>
+          <p> expires in: {result ? result.expires_in : "N/A"} seconds</p>
+
+
+          {result && (
+            <>
+              <h3>Access Token:</h3>
+              <textarea
+                style={{ width: "100%", height: 160 }}
+                value={result.access_token}
+                readOnly
+              />
+
+            
+            </>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
